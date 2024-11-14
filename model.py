18e0572e2dc0b931c3285bf9055f3c45573c28fa
jjaramillo29 <159051418+jjaramillo29@users.noreply.github.com>
2024-11-14 07:39:29 -0500

    Update model.py

--- model.py
+++ model.py
@@ -28,11 +28,12 @@ dfScaled=scaler.fit_transform(df[['open', 'high', 'low', 'close', 'volume',
 """
 ok heres whats happening in this function:
 we have a single dataframe and a sequence length as the inputs
-a sequence is a period of days which defautls to 60
-our inputs is a list that will take a sequence number of rows ofo data from our dataframe and append them as an element in the list
-our output list will append the closing value of the day after the last day in the sequence so sequence i sdays 0-59 output is closing on day 60
-reason being that the ai will look at all data in a given index in inputs and believe that some components of it is leading to the output value being what it is
-and will try to determine what if any correslations can be made, thus determining a trading strategy and when to act based off said strategy
+a sequence is a period of days with the default at 60 days
+our inputs is a list that will take a sequence number of rows of data from our dataframe and append them as an element in the list
+our output list will append the closing value of the day after the last day in the sequence 
+so sequence is days 0-59 output is closing on day 60
+the ai will look at all data in a given index it inputs and believe that some components of it is leading to the output value being what it is
+and will try to determine what if any correlations can be made, thus determining a trading strategy and when to act based off said strategy
 """
 def createSequences(Data: pd.DataFrame, sequenceLength=60) -> pd.DataFrame:
     inputs, output=[],[]
@@ -48,8 +49,8 @@ regarding below section of code:
 we are using a sequential model, which means that it feeds the outputs from each layer directly into the next layer
 we have 5 layers in total, 2 LSTM layers, 2 dropout layers and 1 dense layer
 LSTM layer: (long short term memory) retains some data from each element in our input array that was previously passed in
-this allows it access certain pieces of past sequence data when determining the outputs for the current iteration (within the same training epoch)
-dropout layer: does no atual calculations instead it will randomly set 20% of our 50 neurons in each layer to 0 activitions and therfore 0 outputs
+this allows access to certain pieces of past sequence data when determining the outputs for the current iteration (within the same training epoch)
+dropout layer: does no actual calculations instead it will randomly set 20% of our 50 neurons in each layer to 0 activitions and therfore 0 outputs
 this is useful since it prevents decision making from becoming dependent on a few arbitarily selected neurons instead of using the entire apparatus
 dense: all 50 neurons from the prior layer output into a single neuron which makes a calculation and outputs the predicted price
 """
